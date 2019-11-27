8f8ed72ddb6a752a71eb91115ce040d16f54d676
xiaoming <xm@sina.com>
2019-11-27 20:49:35 +0800

    小程序视图组件

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,35 +8,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-   title:"hello 小程序"
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-     //接受 页面间的传参、发送ajax
-      console.log(utils.formatTime(new Date()));
-
-    //  console.log("页面加载时触发--onload");
-    //  console.log(this.data.title);
-    //  this.setData({
-    //    title:"师傅爱喝的茶叫做乌龙"
-    //  });
+    
   },
-  change1(){
-    this.setData({
-      title:"数据又改了"
-    })
-  },
-  change2(){
-    console.log("asd");
-  },
-  change3(opt){
-    console.log(opt.detail);
-    this.setData({
-      title:opt.detail
-    })
+  scroll(){
+   console.log(111)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
