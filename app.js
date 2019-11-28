674ce4c9d478a17046f76a68963dd5e89440f0b0
xiaoming <xm@sina.com>
2019-11-28 21:14:50 +0800

    小程序API

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
 //app.js
 App({
-  onLaunch:function(){
-
+  onLaunch: function () {
+  
+  },
+  _post(obj){
+    
+    return new Promise((resolve,reject)=>{
+      // 取缓存
+      wx.showLoading({
+        title:'请求中...' ,
+        mask: true
+      });
+      wx.request({
+        method:"POST",
+        url: obj.url,
+        data:obj.data,
+        header: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+          //放token
+        },
+        success: function(res) {
+          /*
+          code: 401  token过期
+          */ 
+         if(res.code === 401){
+           wx.showToast({
+             title: '登录信息',
+             icon: '身份信息过期，请重新登录',
+             complete(){
+              //  跳到登录页
+             }
+           })
+          //  调到登录页面
+         }else if(res.code===0){
+           resolve(res);
+         }
+          wx.hideLoading();
+          
+        },
+        fail:(err)=>{
+          reject(err);
+        }
+      })
+    });
   },
   globalData: {
     userInfo: null,
-    wx:1235456
+    wx:12345
   }
 })
